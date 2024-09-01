e833a5717fcee055e83c2c290d5bdfaaae0b8a87
Abdul Ahad <abdulahadhaq@outlook.com>
2024-09-01 13:46:56 +0300

    first commit

--- simple-NN-in-python.py
+++ simple-NN-in-python.py
@@ -1,14 +1,17 @@
 import numpy as np
 
 x = np.array([[200, 17]])
-W = np.array([[1, -3, 5], [2, 4, -6]])
-b = np.array([[ -1, 1, 2]])
+W = np.array([[1, -3], [2, 4]])
+b = np.array([[-1, 1]])
 
 def dense(a_in, w, b): 
     return (1 / (1 + np.exp(-(np.matmul(a_in, w) + b)))).astype(int)
 
 def sequential(x):
-    a1 = dense(x , W[0], b)
-    a2 = dense(a1, W[2], b)
+    a1 = dense(x, W, b)
+    a2 = dense(a1, W, b)
     f_x = a2
     return f_x
+
+result = sequential(x)
+print(result)
