7dac773b6f00923f2c2e876078cdf949d7fc9a64
Abdul Ahad <abdulahadhaq@outlook.com>
2024-09-02 22:31:47 +0300

    logistic-reg

---
+++ Basic_Neural_Networks/binary-logistic-regression-NN.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.optimizers import Adam
+from sklearn.preprocessing import StandardScaler
+
+#to be continued...
