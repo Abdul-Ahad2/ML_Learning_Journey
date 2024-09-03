5964ebe30a725eb4e0848260eacd034d9ab096f9
Abdul Ahad <abdulahadhaq@outlook.com>
2024-09-03 14:14:35 +0300

    update

--- Basic_Neural_Networks/binary-logistic-regression-NN.py
+++ Basic_Neural_Networks/binary-logistic-regression-NN.py
@@ -1,8 +1,13 @@
+from matplotlib import pyplot as plt
 import tensorflow as tf
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.optimizers import Adam
 from sklearn.preprocessing import StandardScaler
+from sklearn.datasets import load_breast_cancer
 
-#to be continued...
+
+
+
+
